9a02a08bb261fa99b522f0d05e32f11589fb4bdf
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2022-11-05 10:26:42 +0000

    Do some passing tests with default args

--- test/test.js
+++ test/test.js
@@ -1,6 +1,20 @@
 var expect = require("chai").expect;
 var app = require("../src/app");
 
+describe("Time Checker", function () {
+  describe("checks a time is between a start and end time", function () {
+      
+    it("returns true if a time is between times", function () {
+      expect(app.timeChecker()).to.be.true;
+    });
+
+    it("returns false if a time is not between times", function () {
+      expect(app.timeChecker([8,0,0])).to.be.false;
+
+    });
+  });
+});
+
 describe("hello world app", function () {
   it("works", function () {
     expect(app.helloWorld()).to.equal("Hello World");
