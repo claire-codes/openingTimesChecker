835d6680fcdba51d92d4da5b1af16292d6965952
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2022-11-05 10:26:28 +0000

    Setup first draft of function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 exports.helloWorld = function () {
   return "Hello World";
 };
+
+exports.timeChecker = function (currentTime = [9,0,0], startTime = [9, 0, 0], endTime = [17, 0, 0]) {
+    let currentDate = new Date();
+    let startDate = new Date();
+    let endDate = new Date();
+
+    startDate.setHours(startTime[0], startTime[1], startTime[2]);
+    endDate.setHours(endTime[0], endTime[1], endTime[2]);
+    currentDate.setHours(currentTime[0], currentTime[1], currentTime[2]);
+
+    console.log(currentDate);
+    console.log(startDate);
+    console.log(endDate);
+
+    if (currentDate >= startDate && currentDate <= endDate) {
+        return true
+    }
+    return false;
+};
