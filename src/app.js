aea87282b1ea8bd940249c83fdcffba573afa768
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2022-11-06 19:40:38 +0000

    Add Prettier;

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
 let timeChecker = function (
-  currentTime = [8, 0, 0],
-  startTime = [9, 0, 0],
-  endTime = [17, 0, 0]
+    currentTime = [8, 0, 0],
+    startTime = [9, 0, 0],
+    endTime = [17, 0, 0]
 ) {
-  let currentDate = new Date();
-  let startDate = new Date();
-  let endDate = new Date();
+    let currentDate = new Date();
+    let startDate = new Date();
+    let endDate = new Date();
 
-  startDate.setHours(startTime[0], startTime[1], startTime[2]);
-  endDate.setHours(endTime[0], endTime[1], endTime[2]);
-  currentDate.setHours(currentTime[0], currentTime[1], currentTime[2]);
+    startDate.setHours(startTime[0], startTime[1], startTime[2]);
+    endDate.setHours(endTime[0], endTime[1], endTime[2]);
+    currentDate.setHours(currentTime[0], currentTime[1], currentTime[2]);
 
-  // console.log(currentDate);
-  // console.log(startDate);
-  // console.log(endDate);
+    // console.log(currentDate);
+    // console.log(startDate);
+    // console.log(endDate);
 
-  if (currentDate >= startDate && currentDate <= endDate) {
-    return true;
-  }
-  return false;
+    if (currentDate >= startDate && currentDate <= endDate) {
+        return true;
+    }
+    return false;
 };
 
-let storeOpeningTimeChecker = function(day, time = [0,0,0], storeObject) {
+let storeOpeningTimeChecker = function (day, time = [0, 0, 0], storeObject) {
     let times = storeObject.openingTimes;
     if (times) {
         for (let i = 0; i < times.length; ++i) {
-          if (times[i].day === day) {
-            return timeChecker(time, times[i].startTime, times[i].endTime);
-          }
-        };
+            if (times[i].day === day) {
+                return timeChecker(time, times[i].startTime, times[i].endTime);
+            }
+        }
     }
     return false;
-}
+};
 
-let whichStoresAreOpen = function(day, time, storesObject) {
+let whichStoresAreOpen = function (day, time, storesObject) {
     let stores = storesObject.stores;
     let openStores = [];
     if (stores) {
@@ -44,7 +44,7 @@ let whichStoresAreOpen = function(day, time, storesObject) {
         }
     }
     return openStores;
-}
+};
 
 exports.timeChecker = timeChecker;
 exports.storeOpeningTimeChecker = storeOpeningTimeChecker;
--- test/test.js
+++ test/test.js
@@ -4,49 +4,53 @@ var storeObject = require("../data/test/storeTimes.json");
 var storesObject = require("../data/test/stores.json");
 
 describe("Time Checker", function () {
-  describe("checks a time is between a start and end time, good user input", function () {
-    it("returns true if a time is between times", function () {
-      expect(app.timeChecker([12, 0, 0], [11, 0, 0], [13, 0, 0])).to.be.true;
+    describe("checks a time is between a start and end time, good user input", function () {
+        it("returns true if a time is between times", function () {
+            expect(app.timeChecker([12, 0, 0], [11, 0, 0], [13, 0, 0])).to.be
+                .true;
+        });
+
+        it("returns false if a time is not between times", function () {
+            expect(app.timeChecker([8, 0, 0], [11, 0, 0], [14, 0, 0])).to.be
+                .false;
+        });
+
+        it("returns true if all times are the same", function () {
+            expect(app.timeChecker([7, 0, 0], [7, 0, 0], [7, 0, 0])).to.be.true;
+        });
+
+        it("returns true if start and target times are the same", function () {
+            expect(app.timeChecker([7, 0, 0], [7, 0, 0], [8, 0, 0])).to.be.true;
+        });
+
+        it("returns true if end and target times are the same", function () {
+            expect(app.timeChecker([8, 0, 0], [6, 0, 0], [8, 0, 0])).to.be.true;
+        });
     });
 
-    it("returns false if a time is not between times", function () {
-      expect(app.timeChecker([8, 0, 0], [11, 0, 0], [14, 0, 0])).to.be.false;
+    describe("handles rubbish input", function () {
+        it("returns false if no input", function () {
+            expect(app.timeChecker()).to.be.false;
+        });
     });
-
-    it("returns true if all times are the same", function () {
-      expect(app.timeChecker([7, 0, 0], [7, 0, 0], [7, 0, 0])).to.be.true;
-    });
-
-    it("returns true if start and target times are the same", function () {
-      expect(app.timeChecker([7, 0, 0], [7, 0, 0], [8, 0, 0])).to.be.true;
-    });
-
-    it("returns true if end and target times are the same", function () {
-      expect(app.timeChecker([8, 0, 0], [6, 0, 0], [8, 0, 0])).to.be.true;
-    });
-  });
-
-  describe("handles rubbish input", function () {
-    it("returns false if no input", function () {
-      expect(app.timeChecker()).to.be.false;
-    });
-  });
 });
 
 describe("storeOpeningTimeChecker", function () {
-  it("says whether the store is open at a certain time, true", function () {
-    expect(app.storeOpeningTimeChecker("Monday", [16, 0, 0], storeObject)).to.be
-      .true;
-  });
+    it("says whether the store is open at a certain time, true", function () {
+        expect(app.storeOpeningTimeChecker("Monday", [16, 0, 0], storeObject))
+            .to.be.true;
+    });
 
-  it("says whether the store is open at a certain time, false", function () {
-    expect(app.storeOpeningTimeChecker("Monday", [22, 0, 0], storeObject)).to.be
-      .false;
-  });
+    it("says whether the store is open at a certain time, false", function () {
+        expect(app.storeOpeningTimeChecker("Monday", [22, 0, 0], storeObject))
+            .to.be.false;
+    });
 });
 
-describe("checking multiple stores and finding which ones are open", function() {
-    it('returns an array of stores', function() {
-        expect(app.whichStoresAreOpen("Monday", [16, 0, 0], storesObject)).to.eql(['Asda', 'Morrisons']);
-    })
-})
+describe("checking multiple stores and finding which ones are open", function () {
+    it("returns an array of stores", function () {
+        expect(
+            app.whichStoresAreOpen("Monday", [16, 0, 0], storesObject)
+        ).to.eql(["Asda", "Morrisons"]);
+    });
+});
