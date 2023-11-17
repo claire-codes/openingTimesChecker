57bab50f682f5dc0c018b73ca3f8552f3a5bbaae
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2023-11-17 22:09:11 +0000

    Tidying up files

--- code/app.js
+++
@@ -1,5 +0,0 @@
-let button = document.querySelector(".js-button");
-
-button.addEventListener("click", () => {
-    console.log("Hello World");
-});
--- src/components/StoreList.svelte
+++ src/components/StoreList.svelte
@@ -1,12 +1,15 @@
 <script>
-	import storeObject from "../../data/test/stores.json"
+    import storeObject from "../../data/test/stores.json";
+    import { prettyPrintTime } from "../../code/util";
 </script>
 
-
 {#each storeObject.stores as store}
-<h2>{store.name}</h2>
-{#each store.openingTimes as days}
-<div>{days.day}: {days.startTime} - {days.endTime}</div>
-
-{/each}
+    <h2>{store.name}</h2>
+    {#each store.openingTimes as days}
+        <div>
+            {days.day}: {prettyPrintTime(days.startTime)} - {prettyPrintTime(
+                days.endTime
+            )}
+        </div>
+    {/each}
 {/each}
