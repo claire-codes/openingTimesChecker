f4f85967e0d13acefb3a7b828129640824ebc272
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2023-11-17 22:00:46 +0000

    Use ES module syntax

--- test/test.js
+++ test/test.js
@@ -1,68 +1,67 @@
-var expect = require("chai").expect;
-console.log('me');
-// import { timeChecker, storeOpeningTimeChecker, whichStoresAreOpen} from "../src-original/checker.js";
-console.log('too');
-// import storeObject from "../data/test/storeTimes.json" assert { type: "json" };
-// import storesObject from "../data/test/stores.json" assert { type: "json" };
-var checker = require("../src-original/checker");
-var storeObject = require("../data/test/storeTimes.json");
-var storesObject = require("../data/test/stores.json");
+// var expect = require("chai").expect;
+import { expect } from "chai";
+import { timeChecker, storeOpeningTimeChecker, whichStoresAreOpen} from "../code/checker.js";
+import storeObject from "../data/test/storeTimes.json" assert { type: "json" };
+import storesObject from "../data/test/stores.json" assert { type: "json" };
+// import checker from "../code/checker.js";
+// var storeObject = require("../data/test/storeTimes.json");
+// var storesObject = require("../data/test/stores.json");
 
 describe("Time Checker", function () {
     describe("checks a time is between a start and end time, good user input", function () {
         it("returns true if a time is between times", function () {
-            expect(checker.timeChecker([12, 0, 0], [11, 0, 0], [13, 0, 0])).to
+            expect(timeChecker([12, 0, 0], [11, 0, 0], [13, 0, 0])).to
                 .be.true;
         });
 
         it("returns false if a time is before the times", function () {
-            expect(checker.timeChecker([8, 0, 0], [11, 0, 0], [14, 0, 0])).to.be
+            expect(timeChecker([8, 0, 0], [11, 0, 0], [14, 0, 0])).to.be
                 .false;
         });
 
         it("returns false if a time is after the times", function () {
-            expect(checker.timeChecker([20, 0, 0], [11, 0, 0], [14, 0, 0])).to
+            expect(timeChecker([20, 0, 0], [11, 0, 0], [14, 0, 0])).to
                 .be.false;
         });
 
         it("returns true if all times are the same", function () {
-            expect(checker.timeChecker([7, 0, 0], [7, 0, 0], [7, 0, 0])).to.be
+            expect(timeChecker([7, 0, 0], [7, 0, 0], [7, 0, 0])).to.be
                 .true;
         });
 
         it("returns true if start and target times are the same", function () {
-            expect(checker.timeChecker([7, 0, 0], [7, 0, 0], [8, 0, 0])).to.be
+            expect(timeChecker([7, 0, 0], [7, 0, 0], [8, 0, 0])).to.be
                 .true;
         });
 
         it("returns true if end and target times are the same", function () {
-            expect(checker.timeChecker([8, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
+            expect(timeChecker([8, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
                 .true;
         });
 
         it("returns false for midnight 00", function () {
-            expect(checker.timeChecker([0, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
+            expect(timeChecker([0, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
                 .false;
         });
 
         it("returns false for midnight 24", function () {
-            expect(checker.timeChecker([24, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
+            expect(timeChecker([24, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
                 .false;
         });
     });
 
     describe("handles rubbish input", function () {
         it("returns false if no input", function () {
-            expect(checker.timeChecker()).to.be.false;
+            expect(timeChecker()).to.be.false;
         });
 
         it("returns false for false time, positive number like 25", function () {
-            expect(checker.timeChecker([25, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
+            expect(timeChecker([25, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
                 .false;
         });
 
         it("returns false for false time, negative number like -1", function () {
-            expect(checker.timeChecker([-1, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
+            expect(timeChecker([-1, 0, 0], [6, 0, 0], [8, 0, 0])).to.be
                 .false;
         });
     });
@@ -72,7 +71,7 @@ describe("storeOpeningTimeChecker", function () {
     describe("checking a single store", function () {
         it("says whether the store is open at a certain time, true", function () {
             expect(
-                checker.storeOpeningTimeChecker(
+                storeOpeningTimeChecker(
                     "Monday",
                     [16, 0, 0],
                     storeObject
@@ -82,7 +81,7 @@ describe("storeOpeningTimeChecker", function () {
 
         it("says whether the store is open at a certain time, false", function () {
             expect(
-                checker.storeOpeningTimeChecker(
+                storeOpeningTimeChecker(
                     "Monday",
                     [22, 0, 0],
                     storeObject
@@ -94,13 +93,13 @@ describe("storeOpeningTimeChecker", function () {
     describe("checking multiple stores and finding which ones are open", function () {
         it("returns an array of stores", function () {
             expect(
-                checker.whichStoresAreOpen("Monday", [16, 0, 0], storesObject)
+                whichStoresAreOpen("Monday", [16, 0, 0], storesObject)
             ).to.eql(["Asda", "Morrisons"]);
         });
 
         it("returns an empty array of stores if none open", function () {
             expect(
-                checker.whichStoresAreOpen("Monday", [1, 0, 0], storesObject)
+                whichStoresAreOpen("Monday", [1, 0, 0], storesObject)
             ).to.eql([]);
         });
     });
