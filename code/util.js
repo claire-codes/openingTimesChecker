f2c293551a4095f06f904af519b2b3d2ba85b074
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2023-11-17 22:01:15 +0000

    Add util functions

---
+++ code/util.js
@@ -0,0 +1,15 @@
+let prettyPrintTime = function(timeArray) {
+    let hours = timeArray[0];
+    let minutes = timeArray[1];
+    if (hours === 24) { hours = 0}
+    return zeroPad(hours) + ':' + zeroPad(minutes);
+}
+
+let zeroPad = function(num) {
+    let strNum = num.toString();
+    return strNum.length == 1 ? '0' + strNum : strNum;
+    // TODO tests for this
+}
+
+// exports.prettyPrintTime = prettyPrintTime;
+export { prettyPrintTime }
---
+++ test/utils.test.js
@@ -0,0 +1,20 @@
+// var expect = require("chai").expect;
+// var util = require("../code/util");
+import { expect } from "chai";
+import { prettyPrintTime } from "../code/util.js";
+
+describe("pretty prints a time", function () {
+    it("returns true if a time is between times", function () {
+        expect(prettyPrintTime([12, 0, 0])).to.equal("12:00");
+        expect(prettyPrintTime([13, 0, 0])).to.equal("13:00");
+        expect(prettyPrintTime([0, 0, 0])).to.equal("00:00");
+        expect(prettyPrintTime([1, 0, 0])).to.equal("01:00");
+        expect(prettyPrintTime([23, 0, 0])).to.equal("23:00");
+        expect(prettyPrintTime([24, 0, 0])).to.equal("00:00");
+        expect(prettyPrintTime([0, 1, 0])).to.equal("00:01");
+        expect(prettyPrintTime([0, 30, 0])).to.equal("00:30");
+        expect(prettyPrintTime([0, 59, 0])).to.equal("00:59");
+        // Garbage ones
+        // Seconds ones
+    });
+});
