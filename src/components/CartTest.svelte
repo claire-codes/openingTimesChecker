8d9ec4598fb5406a2719975cb52b7907569de49a
Claire Parker-Jones <4523163+claire-codes@users.noreply.github.com>
2023-11-28 13:46:50 +0000

    Proof of concept with Svelte onClick events

---
+++ src/components/CartTest.svelte
@@ -0,0 +1,5 @@
+<script>
+  import { isCartOpen } from '../cartStore';
+</script>
+
+<div>Hello World {$isCartOpen}</div>
---
+++ src/components/CartToggle.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { isCartOpen } from '../cartStore';
+</script>
+
+<!--use "$" to read the store value-->
+<button on:click={() => {console.log("Hello"); isCartOpen.set(!$isCartOpen)}}>Cart {$isCartOpen}</button>
